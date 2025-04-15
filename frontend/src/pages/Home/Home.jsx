ccb509a4e3902652ddb0b497c6b2e68a8dc80638
Daniel Granados <20200008@ricaldone.edu.sv>
2025-04-15 10:24:05 -0600

    home page done

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -13,7 +13,11 @@ import AppImage from "../../assets/app.svg";
 import { MdOutlineExplore } from "react-icons/md";
 import { MdOutlineDownloadForOffline } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
-
+import { FiPlusCircle } from "react-icons/fi";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { MdSupportAgent } from "react-icons/md";
+import { FaRegStar } from "react-icons/fa6";
+import { MdOutlineStorefront } from "react-icons/md";
 
 const Home = () => {
     return (
@@ -105,29 +109,84 @@ const Home = () => {
                     </button>
                 </div>
                 <div className='about-img'>
-                    <img src='https://kia-greccomotors.s3.amazonaws.com/sportage_x_7ba7efe05f_e10688bfe0.webp' alt="App Móvil" />
+                    <img src='https://kia-greccomotors.s3.amazonaws.com/sportage_x_7ba7efe05f_e10688bfe0.webp' alt="Sobre Nosotros" />
                 </div>
             </div>
             {/*Products section*/}
             <div className='catalog'>
                 <div className='catalog-text'>
-                    <h1 className='catalog-tittle'>¿Quiénes somos?</h1>
-                    <p className='catalog-subtittle'>CONÓCENOS</p>
-                    <p className='catalog-txt'><strong>En Hoshi </strong>no solo vendemos autos, ofrecemos<br />
-                    confianza y calidad. Nos especializamos en marcas<br />
-                    japonesas y coreanas reconocidas por su durabilidad,<br />
-                    tecnología y seguridad.<br /><br />
-                    Te brindamos una experiencia de compra sencilla, con<br />
-                    opciones de financiamiento flexibles y un catálogo<br />
-                    diseñado para satisfacer tus necesidades.<br /><br />
-                    <strong>Descubre por qué miles de clientes confían en Hoshi.</strong></p>
+                    <h1 className='catalog-tittle'>Catálogo Destacado</h1>
+                    <p className='catalog-subtittle'>LO MÁS VENDIDO</p>
+                    <p className='catalog-txt'>Descubre autos ideales para toda la familia, con vehículos cómodos,<br />
+                    seguros y amplios, perfectos para tus viajes y actividades diarias.<br />
+                    Encuentra el que se ajuste a tus necesidades y empieza a disfrutar<br />
+                    de cada recorrido.</p>
                         <br />
                     <button class="app-button">
-                        Saber más<FaSearch className="catalog-icon"/>
+                        Ver más<FiPlusCircle className="catalog-icon"/>
                     </button>
                 </div>
-                <div className='catalog-img'>
-                    <img src='https://kia-greccomotors.s3.amazonaws.com/sportage_x_7ba7efe05f_e10688bfe0.webp' alt="App Móvil" />
+                <div class="catalog-img">
+                    <div class="c-img">
+                        <img src="https://www.passporttoyota.com/blogs/716/wp-content/uploads/2020/04/Corolla_Hybrid_013_E8752A42C66E156C23136C861E7A6BAF9B59801D-1500x900.jpg" alt="Catálogo 1" />
+                        <div class="hover-text">Toyota Corolla 2022</div>
+                    </div>
+                    <div class="c-img">
+                        <img src="https://www.passporttoyota.com/blogs/716/wp-content/uploads/2020/04/Corolla_Hybrid_013_E8752A42C66E156C23136C861E7A6BAF9B59801D-1500x900.jpg" alt="Catálogo 2" />
+                        <div class="hover-text">Toyota Corolla 2022</div>
+                    </div>
+                    <div class="c-img">
+                        <img src="https://www.passporttoyota.com/blogs/716/wp-content/uploads/2020/04/Corolla_Hybrid_013_E8752A42C66E156C23136C861E7A6BAF9B59801D-1500x900.jpg" alt="Catálogo 3" />
+                        <div class="hover-text">Toyota Corolla 2022</div>
+                    </div>
+                    <div class="c-img">
+                        <img src="https://www.passporttoyota.com/blogs/716/wp-content/uploads/2020/04/Corolla_Hybrid_013_E8752A42C66E156C23136C861E7A6BAF9B59801D-1500x900.jpg" alt="Catálogo 4" />
+                        <div class="hover-text">Toyota Corolla 2022</div>
+                    </div>
+                    <div class="c-img">
+                        <img src="https://www.passporttoyota.com/blogs/716/wp-content/uploads/2020/04/Corolla_Hybrid_013_E8752A42C66E156C23136C861E7A6BAF9B59801D-1500x900.jpg" alt="Catálogo 5" />
+                        <div class="hover-text">Toyota Corolla 2022</div>
+                    </div>
+                    <div class="c-img">
+                        <img src="https://www.passporttoyota.com/blogs/716/wp-content/uploads/2020/04/Corolla_Hybrid_013_E8752A42C66E156C23136C861E7A6BAF9B59801D-1500x900.jpg" alt="Catálogo 6" />
+                        <div class="hover-text">Toyota Corolla 2022</div>
+                    </div>
+                </div>
+            </div>
+            {/*Extra information section*/}
+            <div className='extra'>
+                <div className='extra-text'>
+                    <h1 className='extra-tittle'>¿Por qué comprar en Hoshi?</h1>
+                    <p className='extra-subtittle'>DESCUBRE</p>
+                </div>
+                <div class="e-cards-container">
+                    <div class="e-card">
+                        <div class="e-icon-container">
+                        <FaRegCheckCircle className="e-react-icon"/>
+                        </div>
+                        <p class="e-text"><strong>Garantizamos calidad y procedencia, </strong>ofreciéndote autos revisados y verificados para brindarte la mayor confianza en tu compra.</p>
+                    </div>
+
+                    <div class="e-card">
+                        <div class="e-icon-container">
+                            <MdSupportAgent className="e-react-icon" />
+                        </div>
+                        <p class="e-text"><strong>Te brindamos atención exclusiva </strong>y dedicada para que encuentres el vehículo que mejor se adapte a tus necesidades y preferencias.</p>
+                    </div>
+
+                    <div class="e-card">
+                        <div class="e-icon-container">
+                        <FaRegStar className="e-react-icon" />
+                        </div>
+                        <p class="e-text"><strong>Te ofrecemos toda la información </strong>de manera clara y sin sorpresas, para que tomes decisiones informadas y seguras.</p>
+                    </div>
+
+                    <div class="e-card">
+                        <div class="e-icon-container">
+                        <MdOutlineStorefront className="e-react-icon" />
+                        </div>
+                        <p class="e-text"><strong>Con nuestra tienda en línea, </strong>puedes comprar tu vehículo de forma rápida y sencilla, desde la comodidad de tu hogar, con opciones de pago flexibles.</p>
+                    </div>
                 </div>
             </div>
         </div>

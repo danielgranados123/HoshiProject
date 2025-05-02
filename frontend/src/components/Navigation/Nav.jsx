33e74ed494adb50229f1927d077bd9cd5de329e0
Daniel Granados <20200008@ricaldone.edu.sv>
2025-05-01 22:40:56 -0600

    some responsive designs

--- frontend/src/components/Navigation/Nav.jsx
+++ frontend/src/components/Navigation/Nav.jsx
@@ -1,35 +1,31 @@
+import { useState } from "react";
 import Shop from "../../assets/shop.svg";
 import Account from "../../assets/account.svg";
 import Logo from "../../assets/logo.svg";
 import "./Nav.css";
 
-const Nav = () => { 
+const Nav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="nav-container">
-            <div className="nav-content"> 
+            <div className="nav-content">
                 <div className="nav-logo">
-                    <img src={Logo} className="logo-img"/>
+                    <img src={Logo} className="logo-img" alt="Logo" />
                 </div>
-                <ul className="nav-menu">
-                    <li>
-                        <a href="/" className="nav-link">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="/about" className="nav-link">Sobre Nosotros</a>
-                    </li>
-                    <li>
-                        <a href="/catalog" className="nav-link">Catálogo</a>
-                    </li>
-                    <li>
-                        <a href="/purchaseHistory" className="nav-link">
-                        <img src={Shop}/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/account" className="nav-link">
-                            <img src={Account}/>
-                        </a>
-                    </li>
+
+                <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </button>
+
+                <ul className={`nav-menu ${isOpen ? "open" : ""}`}>
+                    <li><a href="/" className="nav-link">Inicio</a></li>
+                    <li><a href="/about" className="nav-link">Sobre Nosotros</a></li>
+                    <li><a href="/catalog" className="nav-link">Catálogo</a></li>
+                    <li><a href="/purchaseHistory" className="nav-link"><img src={Shop} alt="Shop" /></a></li>
+                    <li><a href="/login" className="nav-link"><img src={Account} alt="Account" /></a></li>
                 </ul>
             </div>
         </nav>
@@ -37,3 +33,4 @@ const Nav = () => {
 };
 
 export default Nav;
+
